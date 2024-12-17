9648c453b9e24b4b911a0e2a41759a0862ab9437
Thiago <teyextreme@outlook.com>
2024-12-17 17:34:30 -0300

    Atividade Aluta

---
+++ Alura/Filmes/Filme.cs
@@ -0,0 +1,33 @@
+﻿namespace Alura.Filmes;
+class Filme
+{
+    public Filme(string titulo, int duracao, List<string>? elenco)
+    {
+        Titulo = titulo;
+        Duracao = duracao;
+        Elenco = elenco ?? new List<string>();
+    }
+
+    public string Titulo { get; }
+    public int Duracao { get; }
+    public List<string> Elenco { get; set; }
+
+    public void AdicionarElenco(string ator)
+    {
+        Elenco.Add(ator);
+        Console.WriteLine("Elenco adicionado");
+    }
+
+    public void ExibirElenco()
+    {
+        if (Elenco.Count <= 0)
+        {
+            Console.WriteLine("Não há elenco registrado para este filme.");
+            return;
+        }
+        Console.WriteLine("Elenco : ");
+        foreach (var item in Elenco) {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ Alura/Program.cs
@@ -0,0 +1,9 @@
+﻿using Alura.Filmes;
+
+Filme filme1 = new("Poderoso Chefão", 160, new List<string>(){"teste"} );
+Filme filme2 = new("Um sonho de Liberdade", 120, new List<string>() { "teste" });
+Filme filme3 = new("Um sonho de Liberdade", 120, new List<string>() { "teste" });
+Filme filme4 = new("Um sonho de Liberdade", 120, new List<string>() { "teste" });
+Filme filme5 = new("Um sonho de Liberdade", 120, new List<string>() { "teste" });
+
+List<Filme> filmes = new() { filme1, filme2, filme3, filme4, filme5 };
