a4e2ab8ee183a3f4d26580de4c7e952ec4c941c5
Thiago <teyextreme@outlook.com>
2024-12-16 21:01:06 -0300

    Namespaces

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,4 +1,9 @@
-﻿Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+﻿using ScreenSound.Modelos;
+
+Banda linkinPark = new("Linkin Park");
+Banda theBeatles = new("The Beatles");
+
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
 bandasRegistradas.Add("Linkin Park", new List<int> { 10, 8, 6 });
 bandasRegistradas.Add("The Beatles", new List<int>());
 
