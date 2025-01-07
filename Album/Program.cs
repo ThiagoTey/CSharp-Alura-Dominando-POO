c399034e9ba004f521c443d5c0ed41ca96e1e78a
Thiago <teyextreme@outlook.com>
2025-01-07 12:26:38 -0300

    Contador de objetos no Album

---
+++ Album/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Album
+{
+    public Album(string nome)
+    {
+        nome = Nome;
+        ContadorDeObjetos++;
+    }
+
+    public string Nome { get; set; }
+
+    public static int ContadorDeObjetos = 0;
+
+}
+
+public class Program()
+{
+    static void Main()
+    {
+        Album a1 = new Album("Barões da Pisadinha Ao Vivo");
+        Album a2 = new Album("Barões da Pisadinha feat.Anitta");
+        Album a3 = new Album("Barões da Pisadinha no Free Jazz Festival");
+
+        Console.WriteLine($"Total de objetos criados: { Album.ContadorDeObjetos}");
+    }
+}
